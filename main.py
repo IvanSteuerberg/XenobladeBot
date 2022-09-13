23562de7a624272ca08275bd604c7b7bd3d5cd1d
IvanSteuerberg <halloichbinivan@gmail.com>
2022-09-13 17:36:02 +0200

    Zelda bot

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ auth.set_access_token(TOKEN, TOKEN_SECRET)
 client = tweepy.Client(bearer_token=BEARER, consumer_key=APIKEY, consumer_secret=SECRET_KEY, access_token=TOKEN,
                        access_token_secret=TOKEN_SECRET, wait_on_rate_limit=True)
 xenobladeDate = datetime.strptime("29/07/22", '%d/%m/%y')
+zeldaDate = datetime.strptime("12/05/23", "%d/%m/%y")
 
 
 def xenoblade_timer():
@@ -31,11 +32,11 @@ def xenoblade_timer():
         if seconds > 60:
             minutes = int(timeLeft.seconds / 60)
             seconds = seconds % 60
-            leftString = f"{days} days {minutes} minutes and {seconds} seconds"
+            leftString = f" {minutes} minutes and {seconds} seconds"
             if minutes > 60:
                 hours = int(minutes / 60)
                 minutes = minutes % 60
-                leftString = f"{days} days {hours} hours {minutes} minutes and {seconds} seconds"
+                leftString = f"{hours} hours {minutes} minutes and {seconds} seconds"
         texto = f"{leftString} until #XenobladeChronicles3"
         client.create_tweet(text=texto)
 
@@ -51,21 +52,62 @@ def xenoblade_timer2():
         if seconds > 60:
             minutes = int(timeLeft.seconds / 60)
             seconds = seconds % 60
-            leftString = f"{days} días {minutes} minutos y {seconds} segundos"
+            leftString = f"{minutes} minutos y {seconds} segundos"
             if minutes > 60:
                 hours = int(minutes / 60)
                 minutes = minutes % 60
-                leftString = f"{days} días {hours} horas {minutes} minutos y {seconds} segundos"
+                leftString = f"{hours} horas {minutes} minutos y {seconds} segundos"
         texto = f"{leftString} hasta #XenobladeChronicles3"
         client.create_tweet(text=texto)
 
 
+def zelda_timer():
+    if datetime.now() > zeldaDate:
+        client.create_tweet(text="IT'S FUCKING OUT NOW!! #TheLegendOfZelda")
+    else:
+        timeLeft = zeldaDate - datetime.now()
+        days = timeLeft.days
+        seconds = timeLeft.seconds
+        leftString = f"{days} days"
+        if seconds > 60:
+            minutes = int(timeLeft.seconds / 60)
+            seconds = seconds % 60
+            leftString = f" {minutes} minutes and {seconds} seconds"
+            if minutes > 60:
+                hours = int(minutes / 60)
+                minutes = minutes % 60
+                leftString = f"{days} days {hours} hours {minutes} minutes and {seconds} seconds"
+        texto = f"{leftString} until #TheLegendOfZelda Tears of The Kingdom"
+        client.create_tweet(text=texto)
+
+
+def zelda_timer2():
+    if datetime.now() > zeldaDate:
+        client.create_tweet(text="YA ESTÁ PUTO DISPONIBLE!! #TheLegendOfZelda Tears of The Kingdom")
+    else:
+        timeLeft = zeldaDate - datetime.now()
+        days = timeLeft.days
+        seconds = timeLeft.seconds
+        leftString = f"{days} días"
+        if seconds > 60:
+            minutes = int(timeLeft.seconds / 60)
+            seconds = seconds % 60
+            leftString = f"{minutes} minutos y {seconds} segundos"
+            if minutes > 60:
+                hours = int(minutes / 60)
+                minutes = minutes % 60
+                leftString = f"{days} días {hours} horas {minutes} minutos y {seconds} segundos"
+        texto = f"{leftString} hasta #TheLegendOfZelda Tears of The Kingdom"
+        client.create_tweet(text=texto)
+
+
 def main():
-    schedule.every(75).minutes.do(xenoblade_timer2)
+    zelda_timer()
     time.sleep(5)
-    schedule.every(75).minutes.do(xenoblade_timer)
-    schedule.every().day.at('12:00').do(xenoblade_timer)
-    schedule.every().day.at('20:00').do(xenoblade_timer)
+    zelda_timer2()
+    schedule.every(2).hours.do(zelda_timer2)
+    time.sleep(5)
+    schedule.every(2).hours.do(zelda_timer)
 
     while True:
         try:
